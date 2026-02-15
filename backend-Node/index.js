f8b2f38879d987b3a50df214b03bb5a9f03a63c0
tejkonda@gmail.com <tejkonda@gmail.com>
2026-02-15 22:16:23 +0530

    Fix CORS configuration to allow both localhost:5173 and localhost:5174

--- backend-Node/index.js
+++ backend-Node/index.js
@@ -30,7 +30,7 @@ connectDB()
 const app = express()
 
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: ["http://localhost:5173", "http://localhost:5174"],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"]
